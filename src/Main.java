48be87fde5da941473e6a71d773c9b32200da5b6
AGA2020 <hasan9u@gmail.com>
2024-03-17 21:18:57 +0300

    Ödev tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int kilo;
+        double boy, vucut;
+        Scanner girdi2 = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : " );
+        boy = girdi2.nextDouble();
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Lütfen kilonuzu giriniz : " );
+        kilo = girdi.nextInt();
+        vucut = kilo/(boy*boy);
+        System.out.print("Vücut kitle İndeksiniz : " +vucut );
     }
 }
